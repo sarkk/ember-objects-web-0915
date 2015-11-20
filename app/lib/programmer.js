51cba2df44ee37a9531020be1a481ed0a5b515fc
sarkk <korey.aaronson@gmail.com>
2015-11-20 16:25:25 -0500

    some refactoring

--- app/lib/programmer.js
+++ app/lib/programmer.js
@@ -5,52 +5,83 @@ export default Ember.Object.extend({
     return this.get('firstName') + ' ' + this.get('lastName');
   }),
   greet: function(){
-    return `Hi, My name is ${this.get("firstName")} ${this.get("lastName")}. You can call me ${this.nickName}`;
+    return `Hi, My name is ${this.get("fullName")}. You can call me ${this.nickName}`;
   },
-  isOld: Ember.computed("age", function(){
-    if(this.get("age") > 30){
-      return true;
-    } else {
-      return false;
-    }
-  }),
-  wroteRuby: Ember.computed("nickName", function(){
-    if(this.get("nickName") === "Matz"){
-      return true;
-    } else {
-      return false;
-    }
-  }),
+  // isOld: Ember.computed("age", function(){
+  //   if(this.get("age") > 30){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }),
+  isOld: Ember.computed.gte("age", 30),
+  wroteRuby: Ember.computed.equal("authorOf", "Ruby"),
+  // wroteRuby: Ember.computed("nickName", function(){
+  //   if(this.get("nickName") === "Matz"){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }),
   addConference(conference){
-    this.conferences.push(conference);
+    this.get("conferences").pushObject(conference);
   },
-  keyNoteConferences: Ember.computed("conferences.@each.keyNote", function(){
-    var conferences = this.get("conferences");
-    var fullName = this.get("fullName");
-    return conferences.filterBy("keyNote", fullName);
+  // keyNoteConferences: Ember.computed("conferences.@each.keyNote", function(){
+  //   var conferences = this.get("conferences");
+  //   var fullName = this.get("fullName");
+  //   return conferences.filterBy("keyNote", fullName);
+  // }),
+  keyNoteConferences: Ember.computed.filter("conferences.@each.keyNote", function(conference){
+    return conference.keyNote === this.get("fullName");
   }),
-  conferenceNames: Ember.computed("conferences.@each.name", function(){
-    var conferences = this.get("conferences");
-    return conferences.map(function(conference){
-      return conference.get("name");
-    });
-  }),
-  conferenceTotal: Ember.computed("keyNoteConferences", function(){
-    var conferences = this.get("conferences");
-    return conferences.get("length");
-  }),
+
+  // conferenceNames: Ember.computed("conferences.@each.name", function(){
+  //   var conferences = this.get("conferences");
+  //   return conferences.map(function(conference){
+  //     return conference.get("name");
+  //   });
+  // }),
+  // conferenceNames: Ember.computed.map("conferences.@each.name", function(conference){
+  //     return conference.get("name");
+  // }),
+  conferenceNames: Ember.computed.mapBy("conferences", "name"),
+
+  // conferenceTotal: Ember.computed("conferences.[]", function(){
+  //   var conferences = this.get("conferences");
+  //   return conferences.get("length");
+  // }),
+  conferenceTotal: Ember.computed.alias("conferences.length"),
+
   itinerary: Ember.computed("nickName", "conferenceTotal", function(){
     return `${this.get("nickName")} is speaking at ${this.get("conferenceTotal")} conferences`;
   }),
+
   hasValidEmail: Ember.computed("email", function(){
     var regex = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i);
     var email = this.get("email");
-    if (regex.test(email)){
-      return true;
-    } else {
-      return false;
-    }
+    return (regex.test(email));
   }),
+  hasFirstName: Ember.computed.notEmpty("firstName"),
+  haslastName: Ember.computed.notEmpty("lastName"),
+  hasAge: Ember.computed.notEmpty("age"),
+  isValid: Ember.computed.and("hasFirstName", "haslastName", "hasAge", "hasValidEmail"),
+  isInvalid: Ember.computed.not("isValid"),
+
+  // errors: Ember.computed("hasFirstName", "haslastName", "hasAge", "hasValidEmail", function(){
+  //   let errorArray = [];
+  //   let obj = {
+  //     hasFirstName: "firstName cannot be blank",
+  //     hasLastName: "lastName cannot be blank",
+  //     hasAge: "age cannot be blank",
+  //     hasValidEmail: "email must be valid"
+  //   };
+  //   for (var key in obj){
+  //     if (this.get(key)){
+  //       errorArray.push(obj[key]);
+  //     }
+  //   }
+  //   return errorArray;
+  // })
   errors: Ember.computed("firstName", "lastName", "age", "hasValidEmail", function(){
     var errorArray = [];
     if(!this.get("firstName")){
@@ -68,24 +99,6 @@ export default Ember.Object.extend({
     return errorArray;
   }),
   hasErrors: Ember.computed("errors", function(){
-    if (this.get("errors").length === 0){
-      return false;
-    } else {
-      return true;
-    }
-  }),
-  isInvalid: Ember.computed("errors", function(){
-    if (this.get("errors").length === 0){
-      return false;
-    } else {
-      return true;
-    }
-  }),
-  isValid: Ember.computed("isInvalid", function(){
-    if (this.get("errors").length === 0){
-      return true;
-    } else {
-      return false;
-    }
+    return this.get("errors").length !== 0;
   })
 });
